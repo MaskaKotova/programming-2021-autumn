f18085f012efa9d15a1f9cbccf0e5b6624e90984
Котова Мария Евгеньевна <st095989@student.spbu.ru>
2021-09-23 10:32:50 +0300

    Homework 1 task 3

---
+++ 2021.09.19-Homework-1/Project3/Source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int n = 0;
+	int k = 0;
+	cin >> n;
+	cin >> k;
+
+	int m = n % k;
+	cout << m << endl;
+
+	return EXIT_SUCCESS;
+}
