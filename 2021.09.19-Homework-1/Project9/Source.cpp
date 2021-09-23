5de124fa4035c99abd75ea0cb0be60463480163e
MaskaKotova <91019448+MaskaKotova@users.noreply.github.com>
2021-09-23 21:40:38 +0300

    Update Source.cpp

--- 2021.09.19-Homework-1/Project9/Source.cpp
+++ 2021.09.19-Homework-1/Project9/Source.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main(int argc, char* argv[])
 
-int a = 0
-int b = 0
-int c = 0
-cin >> a;
-cin >> b;
+     int a = 0
+     int b = 0
+     int c = 0
+     cin >> a;
+     cin >> b;
 
-c = ((a / b * a + b / a * b) / (a / b + b / a));
+  c = ((a / b * a + b / a * b) / (a / b + b / a));
 
-cout << c << endl;
+  cout << c << endl;
 
 return EXIT_SUCCESS;
 }
