8f1935d2d074800e74d2863aaa9a970e3d65c00d
MaskaKotova <91019448+MaskaKotova@users.noreply.github.com>
2021-09-26 02:01:07 +0300

    Update Source.cpp

--- 2021.09.19-Homework-1/Project8/Source.cpp
+++ 2021.09.19-Homework-1/Project8/Source.cpp
@@ -2,30 +2,31 @@
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) {
 
-int a = 0
-int b = 0
-int c = 0
-int al = 0
-int bl = 0
-int cl = 0
-cin >> a;
-cin >> b;
-cin >> c;
-cin >> a1;
-cin >> b1;
-cin >> c1;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int a1 = 0;
+	int b1 = 0;
+	int c1 = 0;
+	
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> a1;
+	cin >> b1;
+	cin >> c1;
 
-d = 3600;
-f = 60;
-a = a * d;
-a1 = a1 * d;
-b = b * f;
-b1 = b1 * f;
-g = (a1 + b1 + c1) - (a + b + c);
+	int d = 3600;
+	int f = 60;
+	a = a * d;
+	a1 = a1 * d;
+	b = b * f;
+	b1 = b1 * f;
+	int g = (a1 + b1 + c1) - (a + b + c);
 
-cout << g << endl;
+	cout << g << endl;
 
-return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
