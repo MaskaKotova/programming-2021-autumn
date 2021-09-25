4f6eaf3f7b3f1c7bfe95d6d05f9202c0425c9b9f
MaskaKotova <91019448+MaskaKotova@users.noreply.github.com>
2021-09-26 01:34:06 +0300

    Update Source.cpp

--- 2021.09.19-Homework-1/Project5/Source.cpp
+++ 2021.09.19-Homework-1/Project5/Source.cpp
@@ -1,21 +1,22 @@
-#include<iostream>
-
-using namespace std;
-
 int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Russian");
 
-int b = 0
-int a = 0
-int c = 0
-int d = 0
-cin >> a;
+	 int b = 0;
+	 int a = 0;
+	 int c = 0;
+	 int d = 0;
+	 int m = 0;
 
-b = a / 100;
-c = a / 10 - b * 10;
-d = a - b * 100 - c * 10;
-m = b + c + d;
+	cout << "Введите трехзначное число: " << endl;
+	cin >> a;
 
-cout << m << endl;
+	b = a / 100;
+	c = a / 10 - b * 10;
+	d = a - b * 100 - c * 10;
+	m = b + c + d;
 
-return EXIT_SUCCESS;
+	cout <<"Сумма цифр: "<< m << endl;
+
+	return EXIT_SUCCESS;
 }
