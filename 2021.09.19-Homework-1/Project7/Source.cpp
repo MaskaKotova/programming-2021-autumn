00d6aa2d280adbe6ab146a8aa1361ced63e6911f
MaskaKotova <91019448+MaskaKotova@users.noreply.github.com>
2021-12-18 01:25:27 +0300

    Update Source.cpp

--- 2021.09.19-Homework-1/Project7/Source.cpp
+++ 2021.09.19-Homework-1/Project7/Source.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-setlocale(LC_ALL, "Russian");
+  setlocale(LC_ALL, "Russian");
   
-int a = 0;
-int b = 0;
-int c = 0;
+  int a = 0;
+  int b = 0;
+  int c = 0;
   
- cout<<"Значение а: ";
- cin >> a;
- cout<<"Значение а: ";
- cin >> b;
-
-c = a;
-a = b;
-b = c;
-
-cout <<"Новое значение переменной а: "<< a << endl;
-cout <<"Новое значение переменной b: "<< b << endl;
+  cout<<"Значение а: ";
+  cin >> a;
+  cout<<"Значение а: ";
+  cin >> b;
   
-return EXIT_SUCCESS;
+  c = a;
+  a = b;
+  b = c;
+  
+  cout <<"Новое значение переменной а: "<< a << endl;
+  cout <<"Новое значение переменной b: "<< b << endl;
+  
+  return EXIT_SUCCESS;
 }
